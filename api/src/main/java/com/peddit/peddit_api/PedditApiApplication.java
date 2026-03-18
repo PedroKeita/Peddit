a8f8f5806c58f77f2c24058c07746fb1145e88f0
PedroKeita <pedrolucas300805@gmail.com>
2026-03-18 14:56:21 -0300

    initial commit

---
+++ api/src/main/java/com/peddit/peddit_api/PedditApiApplication.java
@@ -0,0 +1,13 @@
+package com.peddit.peddit_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PedditApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PedditApiApplication.class, args);
+	}
+
+}
---
+++ api/src/test/java/com/peddit/peddit_api/PedditApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.peddit.peddit_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PedditApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
