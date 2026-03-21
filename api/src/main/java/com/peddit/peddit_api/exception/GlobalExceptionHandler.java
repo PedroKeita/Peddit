6e248f80d05fd4dc98c4dffcec7f81e085e61a94
PedroKeita <pedrolucas300805@gmail.com>
2026-03-21 19:29:33 -0300

    feature:  add exception for not found resource

--- api/src/main/java/com/peddit/peddit_api/exception/GlobalExceptionHandler.java
+++ api/src/main/java/com/peddit/peddit_api/exception/GlobalExceptionHandler.java
@@ -59,4 +59,17 @@ public class GlobalExceptionHandler {
 
         return ResponseEntity.status(HttpStatus.FORBIDDEN).body(response);
     }
+
+    @ExceptionHandler(ResourceNotFoundException.class)
+    public ResponseEntity<Map<String, Object>> handleNotFound(
+            ResourceNotFoundException ex) {
+
+        Map<String, Object> response = new HashMap<>();
+        response.put("timestamp", LocalDateTime.now());
+        response.put("status", HttpStatus.NOT_FOUND.value());
+        response.put("error", ex.getMessage());
+
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(response);
+    }
+
 }
